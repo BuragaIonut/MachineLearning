406ddabd2bba9acc9807b4debdeb4b1c1b3f6c34
IonutBuraga <BuragaIonut@users.noreply.github.com>
2020-08-24 20:59:08 +0300

    +

---
+++ venv/Data Visualization/titanic.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('Titanic.csv', index_col = 'PassengerId')
+print(df.head())
+
+# plt.hist('Fare', data = df)
+# plt.show()
+# x = np.array(df['Fare'])
+# sns.distplot(x)
+# # plt.show()
+# y = np.array(df['Age'])
+# sns.jointplot('Fare', 'Age', data = df)
+# plt.show()
+sns.violinplot(x='Embarked',y='Age', data = df)
+plt.show()
