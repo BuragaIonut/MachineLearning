86e12f8e224c71d7fa7b0346b206375938d49e1f
IonutBuraga <BuragaIonut@users.noreply.github.com>
2020-10-05 11:04:56 +0300

    +

--- venv/CPC.py
+++ venv/CPC.py
@@ -1,4 +1,5 @@
 cinema = {}
+data = int(input('Data?'))
 for i in range(1,11):
     print(f'Cate filme sunt in sala {i} ?')
     cinema[f'Sala {i}'] = [[],[],[]]
@@ -12,14 +13,16 @@ for i in range(1,11):
     for j in range(1,nr_filme+1):
         film = input(f'Film {j}: ')
         cinema[f'Sala {i}'][1].append(film)
+    for j in range(1, nr_filme + 1):
         ora = input('Ora: ')
         ora = ora.split()
         cinema[f'Sala {i}'][2].append(ora)
 # print(cinema)
 print()
-for i in range(1,11):
-    print(f'<hall{i}>')
-    for j in range(cinema[f'Sala {i}'][0][0]):
-        print(f"<a title='{cinema[f'Sala {i}'][1][j]}' h='{cinema[f'Sala {i}'][2][j][0]}' m='{cinema[f'Sala {i}'][2][j][1]}' />")
-    print(f'</hall{i}>')
+with open(f'cpc-{data}.xml', 'w') as f:
+    for i in range(1,11):
+        print(f'<hall{i}>', file = f)
+        for j in range(cinema[f'Sala {i}'][0][0]):
+            print(f"<a title='{cinema[f'Sala {i}'][1][j]}' h='{cinema[f'Sala {i}'][2][j][0]}' m='{cinema[f'Sala {i}'][2][j][1]}' />", file = f)
+        print(f'</hall{i}>', file = f)
 
